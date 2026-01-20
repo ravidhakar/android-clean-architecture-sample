eabc45ab3a993c46aafd52117f3a10af8ba208c9
Ravindra Dhakar <4330497+ravidhakar@users.noreply.github.com>
2026-01-20 18:19:09 +0530

    Add entity and DTO data models

---
+++ app/src/main/java/com/example/cleanarchitecture/data/local/entity/UserEntity.kt
@@ -0,0 +1,14 @@
+package com.example.cleanarchitecture.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val email: String,
+    val profileImageUrl: String?,
+    val createdAt: Long
+)
