4ff84a4452036614e57a2d16e6367e5f98410abf
Ravindra Dhakar <4330497+ravidhakar@users.noreply.github.com>
2026-01-20 13:06:04 +0530

    Add NetworkModule providing Retrofit, OkHttp, and Gson

--- app/src/main/java/com/ravidhakar/cleanarchsample/core/di/NetworkModule.kt
+++ app/src/main/java/com/ravidhakar/cleanarchsample/core/di/NetworkModule.kt
@@ -1,1 +1,44 @@
-// Content of NetworkModule.kt
+package com.ravidhakar.cleanarchsample.core.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                chain.proceed(request.build())
+            }
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder().create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.example.com/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+}
